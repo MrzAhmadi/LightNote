8306e18f8814ad6d785b9b7d360f87854d41b9b4
Mohammadreza <smrahmadi12@gmail.com>
2023-12-30 15:13:20 +0100

    Add a dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     val composeVersion = "1.5.4"
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-graphics:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
