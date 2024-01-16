4dd9cb2fd89dae6149ed1f7fbc7b625ec8838d6b
MrzAhmadi <MrzAhmadi96@Gmail.com>
2024-01-16 22:00:21 +0100

    Implement some testing for DB

---
+++ app/src/androidTest/java/com/github/mrzahmadi/lightnote/SimpleEntityReadWriteTest.kt
@@ -0,0 +1,62 @@
+package com.github.mrzahmadi.lightnote
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.mrzahmadi.lightnote.data.db.AppDatabase
+import com.github.mrzahmadi.lightnote.data.db.dao.NoteDao
+import com.github.mrzahmadi.lightnote.data.model.Note
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class SimpleEntityReadWriteTest {
+    private lateinit var noteDao: NoteDao
+    private lateinit var db: AppDatabase
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+                context, AppDatabase::class.java).build()
+        noteDao = db.noteDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun writeNoteAndReadInList() {
+        val note = Note(
+            1,
+            title = TEST_TITLE,
+            description = TEST_DESCRIPTION
+        )
+
+        // Test search
+        runBlocking {
+            noteDao.insert(note)
+            val byTitle = noteDao.findByTitle(TEST_TITLE)
+            assertThat(byTitle[0], equalTo(note))
+
+            val byDescription = noteDao.findByDescription(TEST_DESCRIPTION)
+            assertThat(byDescription[0], equalTo(note))
+        }
+    }
+
+    companion object {
+        private const val TEST_TITLE = "TEST_TITLE"
+        private const val TEST_DESCRIPTION = "TEST_DESCRIPTION"
+    }
+}
