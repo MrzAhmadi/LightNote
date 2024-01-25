9ccafa810008e5c92bd7498e2c2936cd70f0a2e2
MrzAhmadi <MrzAhmadi96@Gmail.com>
2024-01-25 16:42:35 +0100

    Save state of home list

--- app/src/main/java/com/github/mrzahmadi/lightnote/view/screen/home/HomeScreen.kt
+++ app/src/main/java/com/github/mrzahmadi/lightnote/view/screen/home/HomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -119,7 +120,9 @@ fun HomeScreen(
             }
         })
 
-    homeViewModel.processIntent(HomeViewIntent.FetchNoteList)
+    LaunchedEffect(state) {
+        homeViewModel.processIntent(HomeViewIntent.FetchNoteList)
+    }
 }
 
 
--- app/src/main/java/com/github/mrzahmadi/lightnote/view/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/github/mrzahmadi/lightnote/view/screen/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.github.mrzahmadi.lightnote.view.screen.home
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.mrzahmadi.lightnote.data.DataState
@@ -13,11 +14,27 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val noteRepository: NoteRepository
+    private val noteRepository: NoteRepository,
+    private val savedStateHandle: SavedStateHandle = SavedStateHandle()
 ) : ViewModel() {
+
 
     private val _state = MutableStateFlow<DataState<List<Note>>>(DataState.Loading(true))
     val state: StateFlow<DataState<List<Note>>> = _state
+
+
+    private fun saveListState(items: List<Note>) {
+        savedStateHandle[NOTE_LIST_KEY] = items
+    }
+
+
+    private fun getSavedListState(): List<Note>? {
+        savedStateHandle.get<List<Note>>(NOTE_LIST_KEY)?.let { notes ->
+            return notes
+        }
+        return null
+    }
+
 
     fun processIntent(intent: HomeViewIntent) {
         when (intent) {
@@ -25,18 +42,37 @@ class HomeViewModel @Inject constructor(
         }
     }
 
+
     private fun getAll() {
         viewModelScope.launch {
-            viewModelScope.launch {
-                _state.value = DataState.Loading(true)
-                try {
-                    val notes = ArrayList(noteRepository.getAll())
-                    _state.value = DataState.Success(notes)
-                } catch (e: Exception) {
-                    _state.value = DataState.Error(e)
-                }
+            val savedState = getSavedListState()?.let { ArrayList(it) }
+            val repoList = ArrayList(noteRepository.getAll())
+            if (savedState.isNullOrEmpty()) {
+                changeState(repoList)
+                saveListState(repoList)
+            } else {
+                if (repoList != savedState) {
+                    changeState(repoList)
+                    saveListState(repoList)
+                } else
+                    changeState(savedState)
             }
         }
+    }
+
+
+    private fun changeState(notes: ArrayList<Note>) {
+        _state.value = DataState.Loading(true)
+        try {
+            _state.value = DataState.Success(notes)
+        } catch (e: Exception) {
+            _state.value = DataState.Error(e)
+        }
+    }
+
+
+    companion object {
+        private const val NOTE_LIST_KEY = "NOTE_LIST_STATE"
     }
 
 }
