8dcda473a046d051145d6a5da214718cad3a5d8e
MrzAhmadi <MrzAhmadi96@Gmail.com>
2024-02-01 18:57:01 +0100

    Add Option Menu generator

--- app/src/main/java/com/github/mrzahmadi/lightnote/data/model/Option.kt
+++ app/src/main/java/com/github/mrzahmadi/lightnote/data/model/Option.kt
@@ -1,10 +1,18 @@
 package com.github.mrzahmadi.lightnote.data.model
 
+import android.content.Context
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import com.github.mrzahmadi.lightnote.R
 
 data class Option(
     val action: Action,
-    val icon: ImageVector,
+    val imageVector: ImageVector? = null,
+    val painter: Painter? = null,
     val title: String,
     val description: String
 ) {
@@ -13,6 +21,48 @@ data class Option(
         ClearData,
         Github,
         About
+    }
+
+    companion object {
+
+        @Composable
+        fun getProfileOptionList(context: Context): ArrayList<Option> {
+            return arrayListOf<Option>().apply {
+                // Add options
+
+                add(
+                    Option(
+                        Action.ClearData,
+                        imageVector = Icons.Outlined.Delete,
+                        painter = null,
+                        context.getString(R.string.item_option_title_clear_data),
+                        context.getString(R.string.item_option_description_clear_data)
+                    )
+                )
+
+                add(
+                    Option(
+                        Action.Github,
+                        imageVector = null,
+                        painter = painterResource(id = R.drawable.github_mark),
+                        context.getString(R.string.item_option_title_clear_data),
+                        context.getString(R.string.item_option_description_clear_data)
+                    )
+                )
+
+                add(
+                    Option(
+                        Action.About,
+                        imageVector = null,
+                        painter = painterResource(id = R.drawable.app_icon),
+                        context.getString(R.string.item_option_title_about),
+                        context.getString(R.string.item_option_description_about)
+                    )
+                )
+
+            }
+
+        }
     }
 
 }
