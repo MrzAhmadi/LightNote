aa5364c413b2021480737793c34643fd33d83a60
MrzAhmadi <MrzAhmadi96@Gmail.com>
2024-04-21 14:26:17 +0200

    update KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     id("com.android.application") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
+        id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
 }
