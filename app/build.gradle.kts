c2bd2f2381a48b1d8652d670589b06140c430565
MrzAhmadi <MrzAhmadi96@Gmail.com>
2024-05-06 23:30:09 +0200

    Increase app Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
     id("com.google.firebase.crashlytics")
 }
 
-val applicationVersionCode = 4
-val applicationVersionName = "0.0051"
+val applicationVersionCode = 5
+val applicationVersionName = "0.0058"
 
 android {
     namespace = "com.github.mrzahmadi.lightnote"
