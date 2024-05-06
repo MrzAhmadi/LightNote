4f97e80eb4a1ead2ea50d303a14437b012dd7b64
MrzAhmadi <MrzAhmadi96@Gmail.com>
2024-05-06 23:20:17 +0200

    Implement SharedPreferencesRepository

---
+++ app/src/main/java/com/github/mrzahmadi/lightnote/data/repository/SharedPreferencesRepository.kt
@@ -0,0 +1,53 @@
+package com.github.mrzahmadi.lightnote.data.repository
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SharedPreferencesRepository @Inject constructor(@ApplicationContext val context: Context) {
+
+    var preferences: SharedPreferences = context.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
+
+    private inline fun SharedPreferences.edit(operation: (SharedPreferences.Editor) -> Unit) {
+        val editor = this.edit()
+        operation(editor)
+        editor.apply()
+    }
+
+    operator fun set(key: String, value: Any?) =
+        when (value) {
+            is String? -> preferences.edit { it.putString(key, value) }
+            is Int -> preferences.edit { it.putInt(key, value) }
+            is Boolean -> preferences.edit { it.putBoolean(key, value) }
+            is Float -> preferences.edit { it.putFloat(key, value) }
+            is Long -> preferences.edit { it.putLong(key, value) }
+            else -> throw UnsupportedOperationException("Not yet implemented")
+        }
+
+    inline operator fun <reified T : Any> get(
+        key: String,
+        defaultValue: T? = null
+    ): T =
+        when (T::class) {
+            String::class -> preferences.getString(key, defaultValue as String? ?: "") as T
+            Int::class -> preferences.getInt(key, defaultValue as? Int ?: -1) as T
+            Boolean::class -> preferences.getBoolean(
+                key,
+                defaultValue as? Boolean ?: false
+            ) as T
+
+            Float::class -> preferences.getFloat(key, defaultValue as? Float ?: -1f) as T
+            Long::class -> preferences.getLong(key, defaultValue as? Long ?: -1) as T
+            else -> throw UnsupportedOperationException("Not yet implemented")
+        }
+
+
+    companion object {
+        private const val APP_PREFERENCES = "app_preferences"
+        const val THEME = "theme"
+    }
+
+}
