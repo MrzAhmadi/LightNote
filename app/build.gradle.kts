0cad875b195b8c92c39bb195944ddde6efe137ae
MrzAhmadi <MrzAhmadi96@Gmail.com>
2024-05-15 17:37:21 +0200

    Remove some unused imports

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
     id("com.google.firebase.crashlytics")
 }
 
-val applicationVersionCode = 6
-val applicationVersionName = "0.0060"
+val applicationVersionCode = 7
+val applicationVersionName = "0.0064"
 
 android {
     namespace = "com.github.mrzahmadi.lightnote"
