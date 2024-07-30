b91b3bd1156136d55dd539fd9d34c928369dadf6
Mohammadreza <MrzAhmadi96@gmail.com>
2024-07-30 13:15:26 +0200

    update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,9 +76,9 @@ hilt {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     //AndroidX
-    val lifecycleVersion = "2.7.0"
+    val lifecycleVersion = "2.8.4"
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.browser:browser:1.8.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
@@ -94,15 +94,15 @@ dependencies {
 
     //Junit and General Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     //Compose
-    val composeVersion = "1.6.7"
-    implementation("androidx.activity:activity-compose:1.9.0")
+    val composeVersion = "1.6.8"
+    implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.compose.ui:ui:$composeVersion")
 //    implementation("androidx.compose.ui:ui-graphics:$composeVersion")
 //    implementation("androidx.compose.foundation:foundation:$composeVersion")
@@ -112,7 +112,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
 
     //Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     //Hilt
     val hiltVersion = "2.51.1"
@@ -151,11 +151,11 @@ dependencies {
 //    implementation("com.github.bumptech.glide:glide:4.16.0")
 
     //Gson
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.11.0")
 
     //Firebase
-     val firebaseAnalyticsVersion = "22.0.0"
-    implementation("com.google.firebase:firebase-crashlytics:19.0.0")
+     val firebaseAnalyticsVersion = "22.0.2"
+    implementation("com.google.firebase:firebase-crashlytics:19.0.3")
     implementation("com.google.firebase:firebase-core:21.1.1")
     implementation("com.google.firebase:firebase-analytics:$firebaseAnalyticsVersion")
     implementation("com.google.firebase:firebase-analytics-ktx:$firebaseAnalyticsVersion")
